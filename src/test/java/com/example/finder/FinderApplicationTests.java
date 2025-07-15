f5cb33bc92ea97deda7398b4fbfb94c580f4b3bf
g-broche <gregory.broche@yahoo.fr>
2025-07-15 16:39:12 +0200

    adding test properties to git and testing CI

--- src/test/java/com/example/finder/FinderApplicationTests.java
+++ src/test/java/com/example/finder/FinderApplicationTests.java
@@ -3,15 +3,16 @@ package com.example.finder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.profiles.active=test")
+//@SpringBootTest(properties = "spring.profiles.active=test")
+@SpringBootTest
 class FinderApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
-	@Test
-	void failBuild(){
-		throw new RuntimeException("force fail");
-	}
+//	@Test
+//	void failBuild(){
+//		throw new RuntimeException("force fail");
+//	}
 }
